231a9b62562515ce549f8fcc54cd7de9ad21fe33
Pat <hoang.patrick@outlook.com>
2023-11-27 15:24:44 +0100

    ajout POST

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
+app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
+app.post("/api/users", movieControllers.postUser);
 app.get("/api/users/:id", movieControllers.getUsersById);
+
 module.exports = app;
