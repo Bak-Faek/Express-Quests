5b9ccce6e87a0fa79d80caa6f994f368c4ec23c1
Pat <hoang.patrick@outlook.com>
2023-11-27 16:49:02 +0100

    ajout PUT

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 app.get("/api/users", movieControllers.getUsers);
 app.post("/api/users", movieControllers.postUser);
 app.get("/api/users/:id", movieControllers.getUsersById);
+app.put("/api/users/:id", movieControllers.updateUser);
 
 module.exports = app;
